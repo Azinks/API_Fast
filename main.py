fa217aa97802c3e7a96335612f6479a63258f7a7
AAJINKYA INGALKAR <aajinkyaingalkar@AAJINKYAs-MacBook-Air.local>
2025-11-23 11:00:09 +0530

    added new routes

--- main.py
+++ main.py
@@ -1,11 +1,27 @@
 from fastapi import FastAPI
+from typing import Optional
 
 app = FastAPI()
 
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"Hello": {'world','new'}}
+
+@app.get('/blog')
+def show(limit,published:bool,sort:Optional[str]=None):
+
+    if published:
+        return{'data':f'{limit} has been published'}
+    else:
+        return{'data':f'{limit} has not been published'}
+
+
+@app.get("/blog/{id}/comments")
+def num(id:int,limit=10):
+
+    return{'data':id}
+
 @app.get('/about')
 def about():
     return {"About" :{"About Page"}}
